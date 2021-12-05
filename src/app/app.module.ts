a385ca3a328f4a0bae23217e365f29b3ea394b6c
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-05 12:37:36 +0330

    create Spetial path for core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { CoreModule } from './core/core.module';
+import { CoreModule } from '@apply-chance/core';
 
 @NgModule({
   declarations: [
