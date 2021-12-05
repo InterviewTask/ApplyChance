e6d2c9dbf832673e35f8cea3223e99908f8940ba
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-05 12:42:22 +0330

    bug fix in interseptor

--- src/app/core/interseptors/jwt.interceptor.ts
+++ src/app/core/interseptors/jwt.interceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjcyZGFjNzBiLTE0NjQtNDQ3YS1hY2YxLTQxZDA1ZjUwYjExNiIsInN1YiI6InRlc3Q4QHRlc3QuY29tIiwiZW1haWwiOiJ0ZXN0OEB0ZXN0LmNvbSIsImp0aSI6ImJhMWM5MTg0LTBhMzgtNDBkOC1hZmQxLTQwMTMzYTgxZjM5YyIsInJvbGUiOiJBcHBsaWNhbnQiLCJuYmYiOjE2Mzg2OTI4ODYsImV4cCI6MTYzODcxNDQ4NiwiaWF0IjoxNjM4NjkyODg2fQ.Ilh70Rz-6_LJg2e5FqiWXaRZgrk3Z04sEhv1uT0grkkgZTkOqd7vinHOVblzbPaYRpk-JAMr_4sOBq2JJ55SZg"
+  token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjcyZGFjNzBiLTE0NjQtNDQ3YS1hY2YxLTQxZDA1ZjUwYjExNiIsInN1YiI6InRlc3Q4QHRlc3QuY29tIiwiZW1haWwiOiJ0ZXN0OEB0ZXN0LmNvbSIsImp0aSI6IjgwMjVmM2M5LWJlODQtNDE2Yi05ZTBiLWE1ODljMjcyZjVkOCIsInJvbGUiOiJBcHBsaWNhbnQiLCJuYmYiOjE2Mzg2OTUxMzYsImV4cCI6MTYzODcxNjczNiwiaWF0IjoxNjM4Njk1MTM2fQ.bhcihYaVGYn78LjlDv_0nWmMZkoNuYliV50f_YEe4XMkpkOWeLQJdWHE6Wcq4M3oxHdXPZFScY93UwxOMZSLEQ"
   constructor() { }
 
 
@@ -15,7 +15,7 @@ export class JwtInterceptor implements HttpInterceptor {
     const headersConfig: any = {
       Accept: 'application/json',
     };
-    headersConfig['Authorization'] = `${this.token}`;
+    headersConfig['Authorization'] = `Bearer ${this.token}`;
     const request = req.clone({ setHeaders: headersConfig });
     return next.handle(request);
   }
