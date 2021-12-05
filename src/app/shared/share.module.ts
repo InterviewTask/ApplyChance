88e102f2707d8a6b9317f8d55ef4d0fc243966ef
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-06 00:08:43 +0330

    install ngSelect

--- src/app/shared/share.module.ts
+++ src/app/shared/share.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeleteDialogComponent, NavbarComponent } from './components';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ApplicationStatuePipe } from './utils/pipes';
 import { FundStatusPipe } from './utils/pipes/fund-status.pipe';
+import { MatButtonModule } from '@angular/material/button';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 
-const pips=[
+const pips = [
   ApplicationStatuePipe,
   FundStatusPipe
 ]
 
-const Mateial=[
+const Mateial = [
   MatDialogModule,
   MatFormFieldModule,
   MatInputModule,
+  MatButtonModule,
   MatDatepickerModule,
   MatNativeDateModule
 ]
 
-const components=[
+const components = [
   NavbarComponent,
   DeleteDialogComponent,
 ]
@@ -35,6 +39,8 @@ const components=[
   ],
   imports: [
     CommonModule,
+    NgSelectModule,
+    FormsModule,
     ...Mateial,
   ],
   exports: [
