f7e2a24b3a64b180d40b7c7fac2cb4a7d0f5b670
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-06 23:16:35 +0330

    add content-type to http headers

--- src/app/core/interseptors/jwt.interceptor.ts
+++ src/app/core/interseptors/jwt.interceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjcyZGFjNzBiLTE0NjQtNDQ3YS1hY2YxLTQxZDA1ZjUwYjExNiIsInN1YiI6InRlc3Q4QHRlc3QuY29tIiwiZW1haWwiOiJ0ZXN0OEB0ZXN0LmNvbSIsImp0aSI6ImQ1MGQ5NmE0LTI2MGMtNGRkYi1hNGQxLWMyMzg3ODY4ZDE0NyIsInJvbGUiOiJBcHBsaWNhbnQiLCJuYmYiOjE2Mzg3MTI4NzQsImV4cCI6MTYzODczNDQ3NCwiaWF0IjoxNjM4NzEyODc0fQ.lkyHgps5tG12fHR-CJmgtVsynhRr1Z8pnjuxjkutzO5llYsXcwDUDW5pm6zetLHS93E677iUpxLuxNnwn9x7Mw"
+  token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjcyZGFjNzBiLTE0NjQtNDQ3YS1hY2YxLTQxZDA1ZjUwYjExNiIsInN1YiI6InRlc3Q4QHRlc3QuY29tIiwiZW1haWwiOiJ0ZXN0OEB0ZXN0LmNvbSIsImp0aSI6IjExY2QzNDc4LTk5ZDItNDU4Ni04NmQ2LTJkZjk0ZGEyMWNjNiIsInJvbGUiOiJBcHBsaWNhbnQiLCJuYmYiOjE2Mzg3OTcyMTIsImV4cCI6MTYzODgxODgxMiwiaWF0IjoxNjM4Nzk3MjEyfQ.bVYWL8z1AKLWcZ0C5GDkNIR0JsQZ_ctudw6HEH_pHsfvtjKjNZmNbUReGXmbzw2qa11DV87Mz41MsK0b2QvZkw"
   constructor() { }
 
 
@@ -14,6 +14,7 @@ export class JwtInterceptor implements HttpInterceptor {
 
     const headersConfig: any = {
       Accept: 'application/json',
+      'content-type':'application/json'
     };
     headersConfig['Authorization'] = `Bearer ${this.token}`;
     const request = req.clone({ setHeaders: headersConfig });
