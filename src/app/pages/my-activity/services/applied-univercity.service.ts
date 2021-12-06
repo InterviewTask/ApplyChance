e7e61538dfcf8f5811fdb5f17fb87a4bb37ba76f
ali_hakimi_njf <ali.hakimi.njf@gmail.com>
2021-12-06 23:24:16 +0330

    complate applied univercity services

--- src/app/pages/my-activity/services/applied-univercity.service.ts
+++ src/app/pages/my-activity/services/applied-univercity.service.ts
@@ -1,16 +1,53 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { Injectable } from '@angular/core';
 import { ApiService } from '@apply-chance/core';
-import { IAppliedUniversity } from '../models';
+import { IAppliedUniversity, ICountry, IDegree,  IMajor, IUniversitiy } from '../models';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class AppliedUnivercityService {
-controller="AppliedUniversity"
-constructor(private api:ApiService) { }
+  constructor(private api: ApiService) { }
 
-getList():Observable<IAppliedUniversity[]>{
-  return this.api.get<IAppliedUniversity[]>
-  (this.controller)
-}
+  getAppliedUniversityList(): Observable<IAppliedUniversity[]> {
+    return this.api.get<IAppliedUniversity[]>
+      ("AppliedUniversity")
+  }
+  deleteAppliedUniversity(id: number): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      }),
+      body: { "id": id }
+    };
+    return this.api.delete<any>
+      ("AppliedUniversity", httpOptions)
+  }
+  addAppliedUniversity(model: IAppliedUniversity): Observable<any> {
+    return this.api.post<Observable<any>>
+      ('AppliedUniversity', model);
+  }
+  editAppliedUniversity(model: IAppliedUniversity): Observable<any> {
+    return this.api.put<Observable<any>>
+      ('AppliedUniversity', model);
+  }
+
+  getCountriesList(): Observable<ICountry[]> {
+    return this.api.get<ICountry[]>
+      ("Country")
+  }
+  getDegreeList(): Observable<IDegree[]> {
+    return this.api.get<IDegree[]>
+      ("Degree")
+  }
+  getUniversitiyList(cuntryId: number | null): Observable<IUniversitiy[]> {
+    if (!cuntryId) return of([]);
+
+    return this.api.get<IUniversitiy[]>
+      ("University/" + cuntryId)
+  }
+  getMajorList(): Observable<IMajor[]> {
+    return this.api.get<IMajor[]>
+      ("Major")
+  }
 }
